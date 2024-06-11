f401174299d609693fe90b9ad85379ae72d149a7
Volodymyr <zmiyov88@gmail.com>
2024-06-11 20:26:53 +0300

    Initial Commit

---
+++ MacOSNotesAdaptableForiOS/MacOSNotesAdaptableForiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOSNotesAdaptableForiOSApp.swift
+//  MacOSNotesAdaptableForiOS
+//
+//  Created by Volodymyr Pysarenko on 11.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MacOSNotesAdaptableForiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
